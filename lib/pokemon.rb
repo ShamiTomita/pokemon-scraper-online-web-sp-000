761ad5ead0f369c202b90e78af51af91c8643d16
Shami Tomita <shamitomita@gmail.com>
2021-04-05 23:05:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,11 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db 
   
-  def initialize(id=nil, name, type, db)
+  def initialize(id=nil, name, type, hp db)
     @id = id
     @name = name 
     @type = type
+    @hp = hp
     @db = db
   end 
   
