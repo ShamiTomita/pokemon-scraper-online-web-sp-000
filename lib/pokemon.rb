8e791292c1e95bc9688a5879f65c82aed9c99878
Shami Tomita <shamitomita@gmail.com>
2021-04-05 23:12:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,6 +18,7 @@ class Pokemon
       
       DB[:conn].execute(sql, self.name, self.type)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
   end 
   
   def self.find 
