d4632196b559a80dc6651f06e89fe3f9fd7cdf0a
Shami Tomita <shamitomita@gmail.com>
2021-04-05 23:24:17 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,12 +18,10 @@ class Pokemon
   end 
   
   def self.find(id, db)
-    sql = <<-SQL 
-    SELECT * FROM pokemon WHERE id = ? 
+    sql = <<-SQL
+      SELECT * FROM pokemon WHERE id = (?);
     SQL
-    
     pokemon = db.execute(sql, [id]).flatten
-    Pokemon.new(id, pokemon[1], pokemon[2], pokemon[3],)
-      
-  end 
+    Pokemon.new(id, pokemon[1], pokemon[2], pokemon[3], db )
+  end
 end
